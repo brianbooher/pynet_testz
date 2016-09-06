7f7cf077ee89284fb86c49f7befa60642c457eab
Brian Booher <brian@boohers.org>
2016-09-05 17:36:46 -0700

    updating changed files

--- class2/telnetimport.py
+++ class2/telnetimport.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import telnetlib
+import time
 
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
@@ -11,9 +12,16 @@ def main():
 	password = '88newclass'
 	
 	remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
-	output = remote_conn.read_until("username:", TELNET_TIMEOUT)
-	remote_conn.close()
+	remote_conn.read_until("sername:", TELNET_TIMEOUT)
+	remote_conn.write(username+'\n')
+        remote_conn.read_until("assword:", TELNET_TIMEOUT)
+        remote_conn.write(password+'\n')
+        remote_conn.read_until("#", TELNET_TIMEOUT)
+        remote_conn.write("show ip int brief"+'\n')
+#### using read_until and flagging on value in command prompt. As part of my standard configuratin, I could speciy a unique tag for the prompt that would always work.
+        output=remote_conn.read_until("#", TELNET_TIMEOUT)
 	print(output)
+        remote_conn.close()
 	
 if __name__ == "__main__":
 	main()
