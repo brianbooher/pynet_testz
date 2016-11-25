41a8b49bcddcdb3b105ba72c8945d6b5124dee07
brianbooher <brian@boohers.org>
2016-11-25 07:16:10 -0500

    class7 ex1
    create class7 exercise 1 script. Will finish editing on server.

---
+++ class7/exercise1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#### import statements
+import pyeapi
+
+#### constants
+MY_VAL = 'whatever'
+OTHER_CONST = 'something else'
+
+#### functions and classes
+def func1(arg1, arg2):
+    do_something
+
+def func2(arg1, arg2):
+    do_something_else
+
+# main function (this is the main execution code for your program)
+def main():
+     # I would define any variables that are specific to this script here
+     my_list_ips = ['192.168.1.1', '2.2.2.2']
+     do_something
+
+     # any variables from main() that need passed into other functions would be passed as arguments
+
+
+if __name__ == "__main__":                    # program execution starts here
+    main()                                                   # first action is to call main function
