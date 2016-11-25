fd3013a0931f12a81c2420a1f8ce33f4c7937277
brianbooher <brian@boohers.org>
2016-11-25 09:03:11 -0500

    modifications to class7 ex 1

--- class7/exercise1.py
+++ class7/exercise1.py
@@ -4,21 +4,20 @@
 import pyeapi
 
 #### constants
-MY_VAL = 'whatever'
-OTHER_CONST = 'something else'
+MY_SWITCH = 'pynet-sw2'
 
 #### functions and classes
-def func1(arg1, arg2):
-    do_something
-
-def func2(arg1, arg2):
-    do_something_else
+def parse_Value(a_device, a_command):
+    a_connection = pyeapi.connect_to(a_device)
+    show_interfaces = a_connection.enable(a_command)
+    all_interfaces = show_interfaces[0]["result"]["interfaces"]
+    for an_interface in all_interfaces:
+        if "Ethernet" in an_interface:
+            in-out-octets = {an_interface :  all_interfaces[an_interface][name]
 
 # main function (this is the main execution code for your program)
 def main():
-     # I would define any variables that are specific to this script here
-     my_list_ips = ['192.168.1.1', '2.2.2.2']
-     do_something
+     parse_Value(MY_SWITCH, 'show interfaces')
 
      # any variables from main() that need passed into other functions would be passed as arguments
 
